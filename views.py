81bb9d1e3b9d5cfff07f861882b61a624f1bf3cd
barricade <2496268087@qq.com>
2022-07-13 17:57:50 +0800

    创建试图login

--- views.py
+++ views.py
@@ -1,5 +1,9 @@
 from djanggo.http import HttpResponse
+from django.shortcut import redirect
 
 def index(request):
     return HttpResponse('index')
 
+
+def login(request):
+    return redirect('/index')
